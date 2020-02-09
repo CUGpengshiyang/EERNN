acb337eb9a4b60b8a5d1e3ee23feef72c2f0f264
lifan <3286150043@qq.com>
2020-02-09 17:23:14 +0800

    删除 choose_lstm 函数(在tensorflow2.1中默认使用CuDNNLSTM)

--- hyper_params.py
+++ hyper_params.py
@@ -12,23 +12,3 @@ LSTM_UNITS = 50
 PREFETCH_SIZE = 128
 SHUFFLE_BUFFER_SIZE = 5120
 MAXLEN = 100
-
-def choose_lstm(lstm_units):
-
-    if tf.test.is_gpu_available():
-        return tf.keras.layers.CuDNNLSTM(
-            name="lstm",
-            units=lstm_units,
-            return_sequences=True,
-            return_state=False,
-            # kernel_regularizer=tf.keras.regularizers.l2(REG_LAMBDA),
-        )
-    else:
-        return tf.keras.layers.LSTM(
-            name='lstm',
-            units=lstm_units,
-            return_sequences=True,
-            return_state = False,
-            dropout=0.1,
-            # kernel_regularizer=tf.keras.regularizers.l2(REG_LAMDA)
-        )
